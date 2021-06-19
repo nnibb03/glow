154d4007b90f39023716773fca9d1325064905ec
nnibb03 <84568556+nnibb03@users.noreply.github.com>
2021-06-19 10:16:48 +0900

    Create hamburger.js

---
+++ js/hamburger.js
@@ -0,0 +1,21 @@
+$("#hamburger-menu").on("click", function(){
+    $("#sp-nav").fadeToggle();
+    $(this).toggleClass("close");
+});
+
+$("#sp-nav ul li a").on("click", function() {
+    $("#sp-nav").fadeToggle();
+    $("#hamburger-menu").removeClass("active");
+});
+
+  
+
+$('a[href^="#"]').click(function() {
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+  
